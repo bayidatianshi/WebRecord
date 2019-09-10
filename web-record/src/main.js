0d2b404af54a7beb051619977418fed1fe599964
zyf <bayidatianshi@gmail.com>
2019-09-10 09:48:09 +0800

    test1

--- web-record/src/main.js
+++ web-record/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import fastclick from 'fastclick'
+import 'common/stylus/index.styl'
 
+fastclick.attach(document.body)
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
